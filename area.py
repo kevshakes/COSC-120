366410e0bf39e39072be75bcf0ac0703bb5faf9e
Kevin Tuei <kevintuei89  @gmail.com>
2025-03-19 17:05:44 +0300

    Added code for area.py

--- area.py
+++ area.py
@@ -1,1 +1,7 @@
 # A Python program to calculate the area of a circle. The program should take the radius as input from the user and print the area. 
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius ** 2
+print("The area of the circle is:", area)
+
